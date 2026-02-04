c938182e50db014a5e768bb2b0eddb81e834a686
Queverity <pryor.baird@ucas-edu.net>
2026-02-04 02:02:15 +0000

    worked somewhat on movie reccomender pseudocode

--- individual_projects/movie_recommender.py
+++ individual_projects/movie_recommender.py
@@ -4,7 +4,7 @@ import csv
 
 def list_parser():
     try:
-        with open("individual_projects\movies_list.csv", mode = "r") as movies_list:
+        with open("individual_projects/movies_list.csv", mode = "r") as movies_list:
             content = csv.reader(movies_list)
             headers = next(content)
             movies = []
@@ -13,7 +13,50 @@ def list_parser():
     except:
         print("Couldn't open the CSV")
     else:
-        for line in movies:
-            print(f"Title: {line['Title']} | Director(s): {line['Director']} | Genre(s): {line['Genre']} | Rating: {line['Rating']} | Length: {line['Length (min)']} | Notable Actors: {line['Notable Actors']}")
+        # for line in movies:
+            # print(f"Title: {line['Title']} | Director(s): {line['Director']} | Genre(s): {line['Genre']} | Rating: {line['Rating']} | Length: {line['Length (min)']} | Notable Actors: {line['Notable Actors']}")
+        return movies
 
-list_parser()
+# Pseudocode
+
+# define function title_filter(keyword):
+    # matching_movies = []
+    # for movie in movies:
+        # check if keyword is anywhere in the movie title (doesn't have to match exactly)
+        # if it is:
+            # matching_movies.append(movie)
+    
+
+# define function director_filter():
+
+# define function genre_filter():
+
+# define function rating_filter():
+
+# define function length_filter():
+
+# define function actor_filter():
+
+# define function filter_combiner():
+
+# define function print_movies():
+
+# define function main_menu():
+
+
+def title_filter():
+    pass
+
+def director_filter():
+    pass
+def genre_filter():
+    pass
+
+def rating_filter():
+    pass
+
+def length_filter():
+    pass
+
+def actor_filter():
+    pass
