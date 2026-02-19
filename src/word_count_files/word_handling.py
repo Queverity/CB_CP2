823feb277fa26daaf3b705afe02ce82f799fe5de
Queverity <pryor.baird@ucas-edu.net>
2026-02-19 03:07:20 +0000

    started working on pseudocode for word counter project

---
+++ src/word_count_files/word_handling.py
@@ -0,0 +1,14 @@
+# CB 1st Word Counter File Handling
+
+# define function parse_document(file_path):
+    # use with open() to parse the text file
+    # save the first line as the main text of the document, save the second line as the date and time of the last update
+    # return document and datetime
+
+# define function update_document(file_path,new_text):
+    # use with open(mode="w") to clear the file, then write each row to the emptied document
+    # parse the document again
+    # return
+
+# for input, each row in the text document will be saved in a list, and to make it s the user will need to hit enter twice to finsh their work, we can have the typing be in a while loop, and if the string the user enters is empty, break the loop
+    
---
+++ src/word_count_files/word_time.py
@@ -0,0 +1,11 @@
+# CB 1st Word Count Time Handling
+
+import datetime as date
+
+current_datetime = date.datetime.now()
+print(current_datetime)
+
+today = date.date.today()
+print(today)
+
+
