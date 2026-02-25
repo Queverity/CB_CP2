33e032c88a7b6941a24921746c57564e4658c1f1
Queverity <pryor.baird@ucas-edu.net>
2026-02-25 09:46:34 -0700

    made basic pseudocode for fractal generaytor

---
+++ src/fractal_generator.py
@@ -0,0 +1,26 @@
+# CB 1st Fractal Generator
+# Note: turtle2img needs to be downloaded
+
+import turtle
+from turtle2img import save_png
+# define function draw_sierpinski(fractal_depth,fractal_color):
+
+
+# define function draw_koch(fractal_depth,fractal_color):
+
+# define function image_saver():
+
+# define function main_menu():
+    # introduce program
+
+    # ask user whether they would like to generate a sierpinski triangle or a koch snowflake
+
+    # ask user for fractal depth
+
+    # ask user for background color and fractal color
+
+    # run fractal generator based on the fractal depth and color inputs
+
+    # ask user if they would like to save it as an image
+
+    # ask user if they would like to continue using the program
