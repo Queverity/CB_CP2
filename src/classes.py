33163be2c0e36e0a7190b9adc0cd5601234799f2
Queverity <pryor.baird@ucas-edu.net>
2026-03-16 09:51:43 -0600

    tooks notes on classes

--- src/classes.py
+++ src/classes.py
@@ -1,45 +1,45 @@
-import turtle
-
-def drawTriangle(points,color,myTurtle):
-    myTurtle.fillcolor(color)
-    myTurtle.up()
-    myTurtle.goto(points[0][0],points[0][1])
-    myTurtle.down()
-    myTurtle.begin_fill()
-    myTurtle.goto(points[1][0],points[1][1])
-    myTurtle.goto(points[2][0],points[2][1])
-    myTurtle.goto(points[0][0],points[0][1])
-    myTurtle.end_fill()
-
-def getMid(p1,p2):
-    return ( (p1[0]+p2[0]) / 2, (p1[1] + p2[1]) / 2)
-
-def sierpinski(points,degree,myTurtle):
-    colormap = ['aqua','aquamarine', 'lavender', 'lightskyblue',
-                'lightskyblue', 'slateblue']
-    drawTriangle(points,colormap[degree],myTurtle)
-    if degree > 0:
-        sierpinski([points[0],
-                        getMid(points[0], points[1]),
-                        getMid(points[0], points[2])],
-                   degree-1, myTurtle)
-        sierpinski([points[1],
-                        getMid(points[0], points[1]),
-                        getMid(points[1], points[2])],
-                   degree-1, myTurtle)
-        sierpinski([points[2],
-                        getMid(points[2], points[1]),
-                        getMid(points[0], points[2])],
-                   degree-1, myTurtle)
-
-def main():
-   myTurtle = turtle.Turtle()
-   myWin = turtle.Screen()
-   myTurtle.speed(0)
-   myPoints = [[200,-100],[0,200],[-200,-100]]
-   sierpinski(myPoints,5,myTurtle)
-   myWin.exitonclick()
-
-main()
+# CB 1st Classes Notes
 
 
+# Example 1
+class Animal:
+    def __init__(self, name, species, age):
+        self.name = name
+        self.species = species
+        self.age = age
+
+    def __str__(self):
+        return f"Name = {self.name}\nSpecies = {self.species}\nAge = {self.age}"
+    
+    def birthday(self):
+        self.age += 1
+
+dog = Animal("Doug","dog",4)
+
+for k,v in vars(dog).items():
+    print(f"{k.capitalize()}: {v}")
+
+print("\n")
+
+print(dog)
+
+dog.birthday()
+
+print(dog)
+
+# Example 2
+class ClassPeriod:
+    def __init__(self,subject,teacher = "Ms. LaRose",room = None):
+        self.subject = subject.title()
+        self.teacher = teacher
+        self.room = room
+
+    def __str__(self):
+        return f"Subject: {self.subject}\nTeacher: {self.teacher}\nRoom: {self.room}"
+    
+first = ClassPeriod("Computer Programming 2", room = 200)
+second = ClassPeriod("Computer Programming 2", room = 200)
+third = ClassPeriod("Computer Science Principles", room = 200)
+sixth = ClassPeriod("Computer Science Principles", room = 200)
+
+print(first, second, third, sixth)
